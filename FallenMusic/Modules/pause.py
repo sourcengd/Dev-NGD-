a2aa1100aa057eed6d081d5a0f89c785616efd65
sourcengd <basharseed126@gmail.com>
2024-05-27 19:47:20 +0300

    Update pause.py

--- FallenMusic/Modules/pause.py
+++ FallenMusic/Modules/pause.py
@@ -37,12 +37,12 @@ async def pause_str(_, message: Message):
 
     if not await is_streaming(message.chat.id):
         return await message.reply_text(
-            "⎊ اكتب كمل او /resume عشان يكمل الاغنيه"
+            " اكتب كمل او /resume عشان يكمل الاغنيه"
         )
 
     await pytgcalls.pause_stream(message.chat.id)
     await stream_off(message.chat.id)
     return await message.reply_text(
-        text=f"⎊ تم ايقاف التشغيل مؤقتاً 🎧\n \n⎊ بواسطة : {message.from_user.mention} ♥",
+        text=f" تم ايقاف التشغيل مؤقتاً 🎧\n \n بواسطة : {message.from_user.mention} ♥",
         reply_markup=close_key,
     )
