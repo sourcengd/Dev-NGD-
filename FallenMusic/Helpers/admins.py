4e9570055e9d418cd0cbd33f02d0c0a542dc8e01
sourcengd <basharseed126@gmail.com>
2024-05-27 20:39:07 +0300

    Update admins.py

--- FallenMusic/Helpers/admins.py
+++ FallenMusic/Helpers/admins.py
@@ -51,7 +51,7 @@ def admin_check(func: Callable) -> Callable:
             return await func(_, message)
         else:
             return await message.reply_text(
-                "⎊ ليس لديك أذونات لإدارة محادثات الفيديو "
+                " ليس لديك صلاحية لإدارة محادثات الفيديو "
             )
 
     return non_admin
@@ -73,7 +73,7 @@ def admin_check_cb(func: Callable) -> Callable:
             return
         if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
             return await query.answer(
-                "⎊ انت لست مشرف 🥺",
+                " انت لست مشرف 🥺",
                 show_alert=True,
             )
 
@@ -84,7 +84,7 @@ def admin_check_cb(func: Callable) -> Callable:
             return await func(_, query)
         else:
             return await query.answer(
-                "⎊ ليس لديك أذونات لإدارة محادثات الفيديو",
+                " ليس لديك صلاحية لإدارة محادثات الفيديو",
                 show_alert=True,
             )
 
