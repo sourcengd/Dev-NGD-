272b09ad5c3dac346043d02c7d39c4b11d4e6fcd
sourcengd <basharseed126@gmail.com>
2024-05-27 19:50:57 +0300

    Update leaveall.py

--- FallenMusic/Modules/leaveall.py
+++ FallenMusic/Modules/leaveall.py
@@ -32,7 +32,7 @@ from FallenMusic import ASS_MENTION, SUNAME, app, app2
 
 @app.on_message(filters.command(["leaveall", "assleaveall"]) | filters.command(["مغادره","مغادرة","مغادره المكالمات","مغادرة المكالمات"],prefixes= ["/", "!","","#"]) & filters.user(OWNER_ID))
 async def ass_leaveall(_, message: Message):
-    lear = await message.reply_text(f"⎊ {ASS_MENTION} جارٍ المغادرة...")
+    lear = await message.reply_text(f" {ASS_MENTION} جارٍ المغادرة...")
     left = 0
     failed = 0
     chats = []
@@ -55,9 +55,9 @@ async def ass_leaveall(_, message: Message):
             failed += 1
     try:
         await lear.edit_text(
-            f"<u>**⎊ {ASS_MENTION} تم المغادره:**</u>\n\n**⎊ خرج من :** `{left}`\n**⎊ فشـل :** `{failed}`"
+            f"<u>** {ASS_MENTION} تم المغادره:**</u>\n\n** خرج من :** `{left}`\n** فشـل :** `{failed}`"
         )
     except:
         await message.reply_text(
-            f"<u>**⎊ {ASS_MENTION} تم المغادره :**</u>\n\n**⎊ خرج من :** `{left}`\n**⎊ فشـل :** `{failed}`"
+            f"<u>** {ASS_MENTION} تم المغادره :**</u>\n\n** خرج من :** `{left}`\n** فشـل :** `{failed}`"
         )
