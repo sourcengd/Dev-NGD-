fa49531558c7964a3bb14a22746501d95676dedc
sourcengd <basharseed126@gmail.com>
2024-05-27 19:33:49 +0300

    Update search.py

--- FallenMusic/Modules/search.py
+++ FallenMusic/Modules/search.py
@@ -27,7 +27,7 @@ from youtube_search import YoutubeSearch
 from FallenMusic import app
 
 
-@app.on_message(filters.command(["search"]) | filters.command(["شغل","ش"],prefixes= ["/", "!","","#"]))
+@app.on_message(filters.command(["search"]) | filters.command(["شغل","تشغيل "],prefixes= ["/", "!","","#"]))
 async def ytsearch(_, message: Message):
     try:
         await message.delete()
@@ -35,18 +35,18 @@ async def ytsearch(_, message: Message):
         pass
     try:
         if len(message.command) < 2:
-            return await message.reply_text("⎊ اكتب اللى عايز تبحث عنه 🎧")
+            return await message.reply_text(" اكتب الي تبي  تبحث عنه 🎧")
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("⎊ جارٍ البحث...")
+        m = await message.reply_text(" جارٍ البحث...")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
         while i < 4:
-            text += f"⎊ العنوان : {results[i]['title']}\n"
-            text += f"⎊ المدة : `{results[i]['duration']}`\n"
-            text += f"⎊ المشاهدات : `{results[i]['views']}`\n"
-            text += f"⎊ القناه : {results[i]['channel']}\n"
-            text += f"⎊ الرابط : https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f" العنوان : {results[i]['title']}\n"
+            text += f" المدة : `{results[i]['duration']}`\n"
+            text += f" المشاهدات : `{results[i]['views']}`\n"
+            text += f" القناه : {results[i]['channel']}\n"
+            text += f" الرابط : https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         key = InlineKeyboardMarkup(
             [
